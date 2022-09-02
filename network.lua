41ae0c8cb13935841a941a8e7385e839a1be3885
Oli <OliverWDGarrett@gmail.com>
2022-09-02 12:30:40 +1200

    Made changes

--- network.lua
+++ network.lua
@@ -1,6 +1,40 @@
 
 
+local json = require("libs.json")
 
 
 
+local network = {}
+
+
+local hst
+
+
+
+function network.host(port)
+    hst = enet.host_create("*", 0)
+end
+
+function network.connect(ip, port)
+    hst = enet.host_create()
+    hst:connect(ip .. ":" .. port)
+end
+
+
+
+
+function network.send(table)
+    local str = json.encode(table)
+    hst:send(str)
+end
+
+
+
+function network.receive()
+    
+end
+
+
+
+return network
 
